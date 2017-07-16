99d48942854f9ceddf54b1b58e59d943c61996e6
Kyle Alexis Sargeant <kasargeant@gmail.com>
2017-07-16 12:27:17 +0200

    Refactors ModelList and ModelCollection as ES2015 classes.

--- src/js/unseen/ModelCollection.js
+++ src/js/unseen/ModelCollection.js
@@ -18,105 +18,211 @@ const EventEmitter = require("event-emitter");
  * * TODO...
  * @class
  */
+class ModelCollection {
 
-/**
- * @param {Array} records - A data array to initially populate this ModelCollection.
- * @param {ModelCollection} [parent] - The parent ModelCollection (if any).
- * @param {number} [parentRefId] - The parent ModelCollection's reference ID for this ModelCollection (if any).
- * @constructor
- */
-function ModelCollection(records=[], parent=null, parentRefId=0) {
+    /**
+     * @param {Array} records - A data array to initially populate this ModelCollection.
+     * @param {ModelCollection} [parent] - The parent ModelCollection (if any).
+     * @param {number} [parentRefId] - The parent ModelCollection's reference ID for this ModelCollection (if any).
+     * @constructor
+     */
+    constructor(records = [], parent = null, parentRefId = 0) {
 
-    // Set internally (or by parent).
-    this._parent = parent; // The parent component.
-    this._id = parentRefId; // The parent's reference ID for this component.
+        // Set internally (or by parent).
+        this._parent = parent; // The parent component.
+        this._id = parentRefId; // The parent's reference ID for this component.
 
-    // Set by user (or default).
-    this.baseClass = null;
-    this.initialize();  // LIFECYCLE CALL: INITIALIZE
+        // Set by user (or default).
+        this.baseClass = null;
+        this.initialize();  // LIFECYCLE CALL: INITIALIZE
 
-    // Calculated from previous internal/user properties.
-    this.models = {};
-    this.length = 0;
+        // Calculated from previous internal/user properties.
+        this.models = {};
+        this.length = 0;
 
-    let i;
-    for(i = 0; i < records.length; i++) {
-        this.models[i] = new this.baseClass(records[i], this, i);
+        let i;
+        for(i = 0; i < records.length; i++) {
+            this.models[i] = new this.baseClass(records[i], this, i);
+        }
+        this.length = i;
+        this._modelCounter = i; // This provides a unique ID for every model.
+
+        // Object.defineProperty(this, "length", {
+        //     get: function() { return this.length; }
+        // });
+
+        this.on("change", function (args) {
+            console.log(`ModelCollection #${this._id}: Model #${args} changed.`);
+            this._emit("change"); // Relay the event forward
+        });
+
+        this.on("view-remove", function (args) {
+            console.log(`ModelCollection #${this._id}: View #${args} changed.`);
+            console.log(`ModelCollection #${this._id}: Removing Model #${args}`);
+            console.log("exists? " + (this.models[args] !== undefined));
+            delete this.models[args];
+            console.log("exists? " + (this.models[args] !== undefined));
+            this._emit("change"); // Relay the event forward
+        });
     }
-    this.length = i;
-    this._modelCounter = i; // This provides a unique ID for every model.
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // LIFECYCLE METHODS
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    // Object.defineProperty(this, "length", {
-    //     get: function() { return this.length; }
-    // });
+    /**
+     * @override
+     */
+    initialize() {}
 
-    this.on("change", function(args) {
-        console.log(`ModelCollection #${this._id}: Model #${args} changed.`);
-        this._emit("change"); // Relay the event forward
-    });
+    /**
+     * @override
+     */
+    finalize() {}
 
-    this.on("view-remove", function(args) {
-        console.log(`ModelCollection #${this._id}: View #${args} changed.`);
-        console.log(`ModelCollection #${this._id}: Removing Model #${args}`);
-        console.log("exists? " + (this.models[args] !== undefined));
-        delete this.models[args];
-        console.log("exists? " + (this.models[args] !== undefined));
-        this._emit("change"); // Relay the event forward
-    });
+    _emit(eventType) {
+        if(this._parent !== null) {
+            // this._parent.dispatchEvent(eventType);
+            this._parent.emit(eventType, this._id);
+        }
+    }
+
+    reset() {
+        this.models = {};
+    }
+
+    get(id) {
+        return this.models[id];
+    }
+
+    set(records) {
+        this.models = {};
+        let i;
+        for(i = 0; i < records.length; i++) {
+            this.models[i] = new this.baseClass(records[i], this, i);
+        }
+        this.length = i;
+        this._modelCounter = i; // This provides a unique ID for every model.
+    }
+
+    add(record) {
+        let id = this._modelCounter++;
+        this.models[id] = new this.baseClass(record, this, id);
+        this.length++;
+    }
+
+    _dump() {
+        for(let id in this.models) {
+            console.log(this.models[id]._dump());
+        }
+    }
+
 }
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// LIFECYCLE METHODS
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-/**
- * @override
- */
-ModelCollection.prototype.initialize = function() {};
-
-/**
- * @override
- */
-ModelCollection.prototype.finalize = function() {};
-
-ModelCollection.prototype._emit = function(eventType) {
-    if(this._parent !== null) {
-        // this._parent.dispatchEvent(eventType);
-        this._parent.emit(eventType, this._id);
-    }
-};
-
-ModelCollection.prototype.reset = function() {
-    this.models = {};
-};
-
-ModelCollection.prototype.get = function(id) {
-    return this.models[id];
-};
-
-ModelCollection.prototype.set = function(records) {
-    this.models = {};
-    let i;
-    for(i = 0; i < records.length; i++) {
-        this.models[i] = new this.baseClass(records[i], this, i);
-    }
-    this.length = i;
-    this._modelCounter = i; // This provides a unique ID for every model.
-};
-
-ModelCollection.prototype.add = function(record) {
-    let id = this._modelCounter++;
-    this.models[id] = new this.baseClass(record, this, id);
-    this.length++;
-};
-
-ModelCollection.prototype._dump = function() {
-    for(let id in this.models) {
-        console.log(this.models[id]._dump());
-    }
-};
 
 EventEmitter(ModelCollection.prototype);
 
 // Exports
 module.exports = ModelCollection;
+
+
+//
+// /**
+//  * @param {Array} records - A data array to initially populate this ModelCollection.
+//  * @param {ModelCollection} [parent] - The parent ModelCollection (if any).
+//  * @param {number} [parentRefId] - The parent ModelCollection's reference ID for this ModelCollection (if any).
+//  * @constructor
+//  */
+// function ModelCollection(records=[], parent=null, parentRefId=0) {
+//
+//     // Set internally (or by parent).
+//     this._parent = parent; // The parent component.
+//     this._id = parentRefId; // The parent's reference ID for this component.
+//
+//     // Set by user (or default).
+//     this.baseClass = null;
+//     this.initialize();  // LIFECYCLE CALL: INITIALIZE
+//
+//     // Calculated from previous internal/user properties.
+//     this.models = {};
+//     this.length = 0;
+//
+//     let i;
+//     for(i = 0; i < records.length; i++) {
+//         this.models[i] = new this.baseClass(records[i], this, i);
+//     }
+//     this.length = i;
+//     this._modelCounter = i; // This provides a unique ID for every model.
+//
+//     // Object.defineProperty(this, "length", {
+//     //     get: function() { return this.length; }
+//     // });
+//
+//     this.on("change", function(args) {
+//         console.log(`ModelCollection #${this._id}: Model #${args} changed.`);
+//         this._emit("change"); // Relay the event forward
+//     });
+//
+//     this.on("view-remove", function(args) {
+//         console.log(`ModelCollection #${this._id}: View #${args} changed.`);
+//         console.log(`ModelCollection #${this._id}: Removing Model #${args}`);
+//         console.log("exists? " + (this.models[args] !== undefined));
+//         delete this.models[args];
+//         console.log("exists? " + (this.models[args] !== undefined));
+//         this._emit("change"); // Relay the event forward
+//     });
+// }
+//
+// //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// // LIFECYCLE METHODS
+// //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// /**
+//  * @override
+//  */
+// ModelCollection.prototype.initialize = function() {};
+//
+// /**
+//  * @override
+//  */
+// ModelCollection.prototype.finalize = function() {};
+//
+// ModelCollection.prototype._emit = function(eventType) {
+//     if(this._parent !== null) {
+//         // this._parent.dispatchEvent(eventType);
+//         this._parent.emit(eventType, this._id);
+//     }
+// };
+//
+// ModelCollection.prototype.reset = function() {
+//     this.models = {};
+// };
+//
+// ModelCollection.prototype.get = function(id) {
+//     return this.models[id];
+// };
+//
+// ModelCollection.prototype.set = function(records) {
+//     this.models = {};
+//     let i;
+//     for(i = 0; i < records.length; i++) {
+//         this.models[i] = new this.baseClass(records[i], this, i);
+//     }
+//     this.length = i;
+//     this._modelCounter = i; // This provides a unique ID for every model.
+// };
+//
+// ModelCollection.prototype.add = function(record) {
+//     let id = this._modelCounter++;
+//     this.models[id] = new this.baseClass(record, this, id);
+//     this.length++;
+// };
+//
+// ModelCollection.prototype._dump = function() {
+//     for(let id in this.models) {
+//         console.log(this.models[id]._dump());
+//     }
+// };
+//
+// EventEmitter(ModelCollection.prototype);
+//
+// // Exports
+// module.exports = ModelCollection;
