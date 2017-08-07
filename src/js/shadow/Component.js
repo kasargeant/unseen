01877e7809e9b78f627c074c8d21e0a9ffc3044c
Kyle Alexis Sargeant <kasargeant@gmail.com>
2017-08-07 23:00:05 +0200

    Updates README docs and standardises Component's layout.

--- src/js/shadow/Component.js
+++ src/js/shadow/Component.js
@@ -95,10 +95,6 @@ class Component {
      */
     remove(key) {}
 
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // UTILITY METHODS
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
     /**
      * Removes...
      * @override
@@ -111,13 +107,38 @@ class Component {
         }
     }
 
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // UTILITY METHODS
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    /**
+     * Sends a message to another component.
+     * @override
+     */
     send(dst, msg) {
         dst.emit("msg", this, msg);
     }
 
+    /**
+     * Receives messages from other components.
+     * @override
+     */
     receive(src, msg) {
         console.log(`Component '${this._id}' received message: ${JSON.stringify(msg)} from: ${src._id}`);
     }
+
+    /**
+     * Fetches data/settings from a remote datasource.
+     * @override
+     */
+    fetch(key, value) {}
+
+    /**
+     * Stores data/settings in a remote datasource.
+     * @override
+     */
+    store(key, value) {}
+
 }
 
 Component.prototype.UUID = 0; // Define component counter on the class.
